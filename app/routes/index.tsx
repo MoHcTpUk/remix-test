f3669dc7c44410ef1ad6aca7de1f94d6cf79ff83
Anton Malay <romory999@gmail.com>
2023-04-27 00:10:56 +0300

    123

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,23 +1,26 @@
-import type { LoaderArgs } from '@remix-run/cloudflare';
+import type { LoaderArgs} from '@remix-run/cloudflare';
 import { json } from '@remix-run/cloudflare';
-import { useLoaderData } from '@remix-run/react/dist/components';
+import { useLoaderData } from '@remix-run/react';
 import { Api } from 'shared/client';
 import { HomePage } from '~/components/pages/HomePage';
 
 export const loader = async ({ request }: LoaderArgs) => {
-  const client = new Api();
-  const response = await client.materials.newsList();
+  // const client = new Api();
+  // const response = await client.materials.newsList();
 
-  if (response.data.data) {
-    return json(response.data.data)
-  }
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+  return json(await res.text());
 
-  return json([]);
+  // if (response.data.data) {
+  //   return json(response.data.data)
+  // }
+
+  // return json([]);
 };
 
 
 export default function Index() {
   const data = useLoaderData<typeof loader>();
   console.warn(data)
-  return <HomePage news={data} />;
+  return <HomePage news={[]} />;
 }
