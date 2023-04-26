895092365d50955ad5c4ed25a011b46b16eb34ca
Anton Malay <romory999@gmail.com>
2023-04-26 22:47:45 +0300

    123

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,23 @@
+import type { LoaderArgs } from '@remix-run/cloudflare';
+import { json } from '@remix-run/cloudflare';
+import { useLoaderData } from '@remix-run/react';
+import { Api } from 'shared/client';
 import { HomePage } from '~/components/pages/HomePage';
 
+export const loader = async ({ request }: LoaderArgs) => {
+  const client = new Api();
+  const response = await client.materials.newsList();
+
+  if (response.data.data) {
+    return json(response.data.data)
+  }
+
+  return json([]);
+};
+
+
 export default function Index() {
-  return <HomePage />;
+  const data = useLoaderData<typeof loader>();
+  console.warn(data)
+  return <HomePage news={data} />;
 }
